1e2329c03dabe1fc262cf5ba908892948ffe4e13
abikena <requenaabigail2@gmail.com>
2023-02-18 11:49:42 -0600

    cambio en archivo js

--- scripts/index.js
+++ scripts/index.js
@@ -41,8 +41,6 @@ stockProductos.forEach((producto) => {
     div.innerHTML = `
     <img src=${producto.img} alt= "">
     <h3>${producto.nombre}</h3>
-    <p>${producto.desc}</p>
-    <p>Talle: ${producto.talle}</p>
     <p class="precioProducto">Precio:$ ${producto.precio}</p>
     <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
     `
